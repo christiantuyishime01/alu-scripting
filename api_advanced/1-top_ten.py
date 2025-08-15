0aacd469ca2049d77203a74c8a07d0fd358f39e6
christiantuyishime01 <c.tuyishime@alustudent.com>
2025-08-15 00:36:50 -0700

    This is my answer

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
+"""Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
+    """Queries the Reddit API and prints the titles of the first 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "linux:subreddit.top.ten:v1.0 (by /u/yourusername)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+    if response.status_code != 200:
         print(None)
+        return
+
+    data = response.json().get("data", {}).get("children", [])
+    if not data:
+        print(None)
+        return
+
+    for post in data:
+        print(post.get("data", {}).get("title"))
+                                                                                
